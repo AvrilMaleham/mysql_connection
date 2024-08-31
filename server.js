3ba9cbf222c35a4edf82ea7695309d54fce2f23c
Avril Maleham <avrilmaleham@outlook.com>
2024-08-31 15:30:59 +1200

    feat: set up express js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+require("dotenv").config();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to my MySQL application." });
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port
+${PORT}.`);
+});
