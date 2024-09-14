8047b117f6cf0d3796aa6c121cbed1e08c05e24c
Avril Maleham <avrilmaleham@outlook.com>
2024-09-14 13:36:06 +1200

    feat: add delete and update routes

--- controllers/groceryItemController.js
+++ controllers/groceryItemController.js
@@ -1,7 +1,7 @@
 "use strict";
 const Models = require("../models");
 
-const getGroceryItems = (res) => {
+const getGroceryItems = (req, res) => {
   Models.GroceryItem.findAll({})
     .then((data) => {
       res.send({ result: 200, data: data });
@@ -12,8 +12,8 @@ const getGroceryItems = (res) => {
     });
 };
 
-const createGroceryItems = (data, res) => {
-  Models.GroceryItem.create(data)
+const createGroceryItems = (req, res) => {
+  Models.GroceryItem.create(req.body)
     .then((data) => {
       res.send({ result: 200, data: data });
     })
@@ -22,7 +22,35 @@ const createGroceryItems = (data, res) => {
       res.send({ result: 500, error: err.message });
     });
 };
+
+const updateGroceryItems = (req, res) => {
+  Models.GroceryItem.update(req.body, {
+    where: { iditems: req.params.id },
+    returning: true,
+  })
+    .then((data) => {
+      res.send({ result: 200, data: data });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send({ result: 500, error: err.message });
+    });
+};
+
+const deleteGroceryItems = (req, res) => {
+  Models.GroceryItem.destroy({ where: { iditems: req.params.id } })
+    .then((data) => {
+      res.send({ result: 200, data: data });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send({ result: 500, error: err.message });
+    });
+};
+
 module.exports = {
   getGroceryItems,
   createGroceryItems,
+  updateGroceryItems,
+  deleteGroceryItems,
 };
--- routes/groceryItemRoutes.js
+++ routes/groceryItemRoutes.js
@@ -3,11 +3,19 @@ const router = express.Router();
 const Controllers = require("../controllers");
 
 router.get("/", (req, res) => {
-  Controllers.groceryItemController.getGroceryItems(res);
+  Controllers.groceryItemController.getGroceryItems(req, res);
 });
 
 router.post("/create", (req, res) => {
-  Controllers.groceryItemController.getGroceryItems(req.body, res);
+  Controllers.groceryItemController.createGroceryItems(req, res);
+});
+
+router.put("/:id", (req, res) => {
+  Controllers.groceryItemController.updateGroceryItems(req, res);
+});
+
+router.delete("/:id", (req, res) => {
+  Controllers.groceryItemController.deleteGroceryItems(req, res);
 });
 
 module.exports = router;
